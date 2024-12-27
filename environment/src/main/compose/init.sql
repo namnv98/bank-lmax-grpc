5c2fdce04e48d1e7d6db888ad9520a00ebe443c6
namnv98 <namnv98@putlook.com.vn>
2024-12-27 09:15:23 +0700

    update scrip test

---
+++ environment/src/main/compose/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE balances
+(
+    id          BIGINT PRIMARY KEY,
+    amount      BIGINT  NOT NULL DEFAULT 0,
+    precision   INT     NOT NULL DEFAULT 2,
+    active      BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE snapshots
+(
+    id  VARCHAR(36) PRIMARY KEY,
+    value VARCHAR(255) NOT NULL
+);
+
+INSERT INTO snapshots (id, value) VALUES ('LAST_KAFKA_OFFSET', '-1');
+INSERT INTO snapshots (id, value) VALUES ('LAST_BALANCE_ID', '0');
+
+INSERT INTO balances (id, amount, precision, active) VALUES (1, 0, 2, true);
+INSERT INTO balances (id, amount, precision, active) VALUES (2, 0, 2, true);
